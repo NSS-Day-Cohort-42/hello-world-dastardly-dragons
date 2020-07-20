305b7a1b953d5936f0042af0cd3974eec3b97b60
Chris Johnson <christopherjohnson1@gmail.com>
2020-07-20 11:51:30 -0500

    landmarks dynamically print

--- scripts/switzerland/main.js
+++ scripts/switzerland/main.js
@@ -1,7 +1,9 @@
 import {cityList} from './cities/citiesList.js'
+import {landmarksList} from './landmarks/landmarksList.js'
 
 const pageBuilder = () => {
   cityList()
+  landmarksList()
 }
 
 pageBuilder()
