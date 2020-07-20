5204410c422335d374a28f2041b075b14d7ffd2c
Chris Johnson <christopherjohnson1@gmail.com>
2020-07-20 14:49:37 -0500

    Im an idiot

--- scripts/switzerland/cities/cities.js
+++ scripts/switzerland/cities/cities.js
@@ -1,5 +1,5 @@
 export const citiesAsHtml = (cityObj) => {
   return `
-        <li class="city__name">${cityObj.name} pop: ${cityObj.pop}</li>
+        <li class="city__name">${cityObj.name}, pop. ${cityObj.pop}</li>
   `
 }
