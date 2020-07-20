664569e9e44acaebf27c970998016284ccfb69cb
Mark Skinner <jmskinne@gmail.com>
2020-07-20 11:46:59 -0500

    working on landmarks

---
+++ scripts/australia/Landmarks/LandmarkHTMLConverter.js
@@ -0,0 +1,6 @@
+export const LandmarksHTML = (landmarkObj) => {
+    return `
+        <li>${landmarkObj.name} pop. ${landmarkObj.location}</li>
+                     
+    `
+}
---
+++ scripts/australia/Landmarks/LandmarkList.js
@@ -0,0 +1,23 @@
+import {useLandmark} from "./Landmark.js"
+import {landmarkHTML} from "./CityHTMLConverter.js"
+
+export const CityToDOM = () => {
+    const contentElement = document.querySelector('.country-content')
+
+    const cityArray = useCity()
+
+    let cityHTMLRep = ""
+
+    for(const currentCityObj of cityArray) {
+        cityHTMLRep += CityHTML(currentCityObj)
+    }
+    contentElement.innerHTML += `
+        <section class="card">
+            <h3>Major Cities</h3>
+                <ul> ${cityHTMLRep}
+                </ul>
+        </section>
+
+    `
+
+}
