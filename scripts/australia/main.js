415e2eb954e83613952c8500229c790ca3e0d8b7
Mark Skinner <jmskinne@gmail.com>
2020-07-20 13:19:39 -0500

    added heads of state as well

--- scripts/australia/main.js
+++ scripts/australia/main.js
@@ -1,6 +1,8 @@
 import {CityToDOM} from "./Cities/CityList.js"
 import { LandmarksToDOM } from "./Landmarks/LandmarkList.js"
+import { HeadsOfStateToDOM } from "./headsOfState/HeadsOfStateList.js"
 
 
 CityToDOM()
 LandmarksToDOM()
+HeadsOfStateToDOM()
