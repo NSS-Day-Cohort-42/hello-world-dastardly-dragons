5cc995b0adf055afa033f203dd0f35a9abb93140
justha <nganha42@gmail.com>
2020-07-21 10:26:42 -0500

    added header modules for new zealand

---
+++ scripts/newZealand/header/headersDataProvider.js
@@ -0,0 +1,10 @@
+const headerCollection = [
+    {
+        countryName: "New Zealand",
+        countryImage: "/images/blue_door.jpg"
+    }
+] 
+
+export const useHeaders = () => {
+    return headerCollection.slice()
+}
