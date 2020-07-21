5b8f2e3d064e18fe1fbd33498559a825cb9dc907
Mark Skinner <jmskinne@gmail.com>
2020-07-21 10:31:50 -0500

    dynamically load header image and country name

---
+++ scripts/australia/headerToHTML/headerDataProvider.js
@@ -0,0 +1,10 @@
+const australiaHeaderContent = [
+    {
+        country: "Australia",
+        countryImage:"/images/sydney.jpg"
+    }
+]
+
+export const australiaHeader = () => {
+    return australiaHeaderContent.slice()
+}
