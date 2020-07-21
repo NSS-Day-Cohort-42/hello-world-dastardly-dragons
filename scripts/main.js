d37f54e471567d54c7ea8e17bc59ebca034a0dd4
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-07-21 11:08:56 -0500

    page builder for switz and sweden

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+import {switzerlandPageBuilder} from './switzerland/switzerland.js'
+import { swedenPageBuilder } from './sweden/sweden.js'
+
+switzerlandPageBuilder()
+swedenPageBuilder()
--- scripts/sweden/sweden.js
+++ scripts/sweden/sweden.js
@@ -3,8 +3,10 @@ import { landmarkList } from './landmarks/landmarksList.js'
 import { headList } from './headsOfState/headList.js'
 import {headerList} from './headerList.js'
 
-headerList()
 
-cityList()
-landmarkList()
-headList()
+export const swedenPageBuilder = () => {
+    headerList()
+    cityList()
+    landmarkList()
+    headList()
+}
